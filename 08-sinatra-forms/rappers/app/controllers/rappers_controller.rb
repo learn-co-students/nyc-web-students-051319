07ac026ce868a2311e7e096d0d49d4a7052f97bb
Amy Cen <cenamy@gmail.com>
2019-06-04 14:31:35 -0400

    done

--- 08-sinatra-forms/rappers/app/controllers/rappers_controller.rb
+++ 08-sinatra-forms/rappers/app/controllers/rappers_controller.rb
@@ -11,16 +11,50 @@ class RappersController < ApplicationController
     erb :"rappers/new"
   end
 
+
+#show update
+  get '/rappers/:id/update' do
+    @rapper = Rapper.find(params[:id])
+    erb :"rappers/update"
+  end
+
 #show action
   get '/rappers/:id' do
     @rapper = Rapper.find(params[:id])
     erb :"rappers/show"
-    end
+  end
 
 #create action
-post '/rappers' do
-rapper = Rapper.create(name: params[:name], age: params[:age], fire: params[:fire])
-redirect "/rappers/#{rapper.id}"
-end
+  post '/rappers' do
+    rapper = Rapper.create(name: params[:name], age: params[:age], fire: params[:fire])
+    redirect "/rappers/#{rapper.id}"
+  end
 
+#update action
+  patch '/rappers/:id' do
+    params.keys.each do |key|
+
+      if key.include?("method")
+        
+      else
+        if !params[key.to_sym].empty?
+          Rapper.update(params[:id], key.to_sym => params[key.to_sym])
+        end
+      end
+    end
+
+    # Rapper.update(params[:id], name: params[:name]) if !params[:name].empty?
+
+    # Rapper.update(params[:id], age: params[:age]) if !params[:age].empty?
+
+    # Rapper.update(params[:id], fire: params[:fire]) if !params[:fire].empty?
+
+    redirect "/rappers/#{params[:id]}"
+  end
+
+  #delete action
+    delete '/rappers/:id' do
+      Rapper.delete(params[:id])
+      redirect "/rappers"
+    end
 end
