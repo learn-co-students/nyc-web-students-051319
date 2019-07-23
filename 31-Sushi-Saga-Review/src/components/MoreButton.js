bd2b14e05a7a88fee36c44df2e47477b060d6b0e
Charlie Russo <cparusso@gmail.com>
2019-07-23 16:20:42 -0400

    Corrected Files

--- 31-Sushi-Saga-Review/src/components/MoreButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MoreButton = (props) => {
-    return <button onClick={ props.moreSushi }>
-            More sushi!
-          </button>
-}
-
-export default MoreButton
